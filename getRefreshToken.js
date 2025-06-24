49c982dd298ba1c6206f38a67772243ed00d1d3a
Sagar <sagar90160@gmail.com>
2025-06-24 12:23:24 +0530

    full report changes

--- getRefreshToken.js
+++ getRefreshToken.js
@@ -1,13 +1,13 @@
+require("dotenv").config();
 const { google } = require("googleapis");
 
-// ✅ Yahan apna code paste kar jo Google ne URL ke baad diya tha
-const code = "4/0AUJR-x6ss4HipNKfpwD2V6F_WYVB304prEmdJ-RJl27AvQVFXl_uuZnQ75t5QjdDk0sFgg";
-
 const oAuth2Client = new google.auth.OAuth2(
-  "614898504214-i6lb82gcs1h1d1lh2i1i232bcet2v7c6.apps.googleusercontent.com",
-  "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",
-  "https://crmbackend-yho0.onrender.com/oauth/google/callback"
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
 );
+
+const code = process.env.GOOGLE_AUTH_CODE;
 
 async function getToken() {
   try {
@@ -15,7 +15,7 @@ async function getToken() {
     console.log("✅ Access Token:", tokens.access_token);
     console.log("🔁 Refresh Token:", tokens.refresh_token);
   } catch (err) {
-    console.error("❌ Error getting tokens:", err);
+    console.error("❌ Error getting tokens:", err.message);
   }
 }
 
